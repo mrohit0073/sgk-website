3f8bad02884d29e19a5a8517e2cf9eba523ddb4a
mrohit0073 <146334183+mrohit0073@users.noreply.github.com>
2026-02-01 13:36:53 +0530

    Create isp-config.js

---
+++ config/isp-config.js
@@ -0,0 +1,18 @@
+// config/isp-config.js
+// Edit ONLY this file to spin up a new ISP site quickly.
+
+window.ISP_TEMPLATE = {
+  defaultBrand: "Your ISP Name",
+  defaultCities: "Mumbai, Thane, Navi Mumbai",
+  defaultPhone: "919876543210",
+  defaultEmail: "support@example.com",
+  defaultAddress: "Your Office Address",
+  defaultUpi: "yourupi@bank",
+  defaultWhatsapp: "", // digits only preferred (e.g., 9198xxxxxxx). If empty, phone will be used.
+  defaultSelectedCity: "", // Optional. If set and exists in cities, it will be pre-selected.
+  defaultSocials: {
+    facebook: "",
+    instagram: "",
+    twitter: ""
+  }
+};
